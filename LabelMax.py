ca02d9c0d9eae30f48453f33b24bb7d6e8963752
Jordan Morales <52443263+Jordan-Morales@users.noreply.github.com>
2023-04-10 19:07:01 -0500

    oAuth and Data Request Test
    Overview: Successfully pulled label data from gmail account via oAuth.
    
    Requirements: Gmail API credentials.json to import and process baseline login.
    Non-secure app access approval.
    
    Need: Security increase.
    oAuth login portal.

--- LabelMax.py
+++ LabelMax.py
@@ -1,41 +1,52 @@
-#import the necessary libraries and any associated or required; google-api-python-client, numpy, pandas, nltk, scikit-learn, gensim, tensorflow, flask.
-import os
-import sys
-import json
-import time
-import datetime
-import numpy as np
-import pandas as pd
-import nltk
-import gensim
-import tensorflow as tf
-# pull out important modules from imported libraries
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-# google-api-python-client  import
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from oauth2client.tools import argparser
-# nltk import
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-# scikit-learn import
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-# gensim import
-from gensim.models import Word2Vec
-# tensorflow import
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-# flask import
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-# import a dotenv type library to hide the API key
-from dotenv import load_dotenv
-# Path: LabelMax.py
-# Define the flask app
-app = Flask(__name__)
-CORS(app)
-# Path: LabelMax.py
+# Import and prep dotenv
+#import os
+#from dotenv import load_dotenv
 
-# Define the global variables.
+## Load environment variables from the .env file
+#load_dotenv()
+
+## Access the environment variables in your code
+#api_key = os.getenv('API_KEY')
+#debug = os.getenv('DEBUG') == 'True'
+
+### Import the necessary libraries and any associated or required; google-api-python-client, numpy, pandas, nltk, scikit-learn, gensim, tensorflow, flask.
+##import os
+##import sys
+##import json
+##import time
+##import datetime
+##import numpy as np
+##import pandas as pd
+##import nltk
+##import gensim
+##import tensorflow as tf
+### pull out important modules from imported libraries
+##from flask import flask, request, jsonify
+##from flask_cors import cors
+### google-api-python-client  import
+##from googleapiclient.discovery import build
+##from googleapiclient.errors import httperror
+##from oauth2client.tools import argparser
+### nltk import
+##from nltk.corpus import stopwords
+##from nltk.stem import wordnetlemmatizer
+### scikit-learn import
+##from sklearn.feature_extraction.text import tfidfvectorizer
+##from sklearn.metrics.pairwise import cosine_similarity
+### gensim import
+##from gensim.models import word2vec
+### tensorflow import
+##from tensorflow.keras.preprocessing.text import tokenizer
+##from tensorflow.keras.preprocessing.sequence import pad_sequences
+### flask import
+##from flask import flask, request, jsonify
+##from flask_cors import cors
+### import a dotenv type library to hide the api key
+##from dotenv import load_dotenv
+### path: labelmax.py
+### define the flask app
+##app = flask(__name__)
+##cors(app)
+### path: labelmax.py
+
+### define the global variables.
